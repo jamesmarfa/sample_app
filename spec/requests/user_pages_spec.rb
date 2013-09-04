ec10331e7763a2d015f168865ff6b7785dd671f8
jamesmarfa <jamesmarfa@gmail.com>
2013-09-04 12:33:01 +0800

    modified flash in application.html.erb

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -22,6 +22,12 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+        it { should have_title('Sign Up') }
+        it { should have_selector('div.alert.alert-error', text: 'The form contains') }
+      end
     end
 
     describe "with valid information" do
