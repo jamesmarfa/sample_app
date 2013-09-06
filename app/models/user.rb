b9f9ab6af11fbafc7c63dee2d6224e6819bd5302
jamesmarfa <jamesmarfa@gmail.com>
2013-09-06 13:33:40 +0800

    Add user micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,19 @@
 class User < ActiveRecord::Base
+  # Association
+  has_many :microposts, dependent: :destroy
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+  has_secure_password
 
   before_save { self.email = email.downcase }
   before_create :create_remember_token
-  has_secure_password
-  validates :password, length: { minimum: 6 }
+  
+  def feed
+    microposts
+  end
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
